2536c709a054a6e1c108b13259b98e8e1b0f582d
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-11 20:40:04 +0100

    monitoring

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,25 +1,25 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-  remove_default_node_pool = true
+  # remove_default_node_pool = true
   initial_node_count       = 1
-}
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  cluster    = google_container_cluster.primary.name
-  location   = var.region
-
-  initial_node_count = 1
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 2
-  }
-
-  node_config {
+    node_config {
     machine_type = "e2-micro"
     disk_type    = "pd-ssd"
     disk_size_gb = 10
   }
 }
+
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = "primary-node-pool"
+#   cluster    = google_container_cluster.primary.name
+#   location   = var.region
+
+#   initial_node_count = 1
+
+#   autoscaling {
+#     min_node_count = 1
+#     max_node_count = 2
+#   }
+# }
